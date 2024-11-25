583f7aee7a0a7f7b4028b4feb018eee95ed8a041
sgtgarrido3rm <sgtgarrido3rm@gmail.com>
2024-11-24 23:44:43 -0300

    Comentários adicionados

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,22 @@
+#############################################################################
+# Autor: Luis Olavo Garrido                                                 #
+# E-Mail: sgtgarrido3rm@gmail.com                                           #
+# Data: 2024nov24                                                           #
+# Instituição: Cruzeiro do Sul Virtual (Universidade Positivo)              #
+# Disciplina: Atividades de Extensão - Integração de Competências em IA II  #
+# https://github.com/sgtgarrido3rm/querenciasaude                           #
+#############################################################################
+
+# importação da biblioteca do telegram
 import telebot
 
+# token exclusivo gerado pelo bot do telegram para conectar ao Telegram
 CHAVE_API = "7912655832:AAEgz6ntlEq3iSBfF_cfQsLR_Dq5do9AzOo"
 
+# objeto que conecta ao telegram com o uso da cheve
 bot = telebot.TeleBot(CHAVE_API)
 
+### INÍCIO FUNÇÕES SECUNDÁRIAS ###
 @bot.message_handler(commands=["baixa"])
 def baixa(mensagem):
     texto = """
@@ -46,12 +59,15 @@ def pressao(mensagem):
     /alta Alta
     /baixa Baixa"""
     bot.send_message(mensagem.chat.id, texto)
+### FIM FUNÇÕES SECUNDÁRIAS ###
 
-
+# função retorna verdadeiro sempre, porque deve ser executada a função [responder] independente do que for digitado
 def verificar(mensagem):
     return True
 
+# decorator que pega a mensagem do [bot.polling()] e após receber [True] libera para chamar a função [responder]
 @bot.message_handler(func=verificar)
+# função padrão do chatbot
 def responder(mensagem):
     texto = """
     Escolha uma opção para continuar (Clique no item):
@@ -60,4 +76,5 @@ def responder(mensagem):
 Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
     bot.reply_to(mensagem, texto)
 
+# deixa o código em loop para que fique sempre ativo o chatbot
 bot.polling()
